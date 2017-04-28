a3099b231a80aa1f932f11b7bd239c8e72ec546f
Phil Wade <phil@whoisstudio.com>
2017-04-28 15:33:02 -0400

    Put built stuff into a directory

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc resume.md -f markdown -V geometry:margin=1in -s -o PhilipWadeResume.pdf
-pandoc resume.md -f markdown -t html -s -o index.html
+pandoc resume.md -f markdown -V geometry:margin=1in -s -o build/PhilipWadeResume.pdf
+pandoc resume.md -f markdown -t html -s -o build/index.html
