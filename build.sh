8c7ce45a8e1df9f5a33d7e23d371a4ea4082c8c6
Phil Wade <phil@whoisstudio.com>
2017-04-28 15:36:32 -0400

    Adjust PDF margins

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc resume.md -f markdown -V geometry:margin=1in -s -o build/PhilipWadeResume.pdf
+pandoc resume.md -f markdown -V geometry:margin=0.75in -s -o build/PhilipWadeResume.pdf
 pandoc resume.md -f markdown -t html -s -o build/index.html
